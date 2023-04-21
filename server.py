265a6381b1453cc7c9ce764409fca97c5c512934
Edson Neto <72220739+emn2@users.noreply.github.com>
2023-04-21 12:41:19 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 import socket 
+import functions
 
 # Definir o endereco IP e a porta do servidor
 IP = socket.gethostbyname(socket.gethostname())
@@ -23,14 +24,15 @@ while True:
         while True:
             # Recebe o próximo pacote de dados
             data, address = serverSocket.recvfrom(1024)
-            # Se o pacote recebido for vazio, termina a transmissão
-            if not data:
-                break
-           
+
             # Convertendo os dados de bytes para string
             dataString = data.decode()
             print(f'Recebido: {data.decode()}')
             #dataInt = int(dataString)
+            
+            # Se o pacote recebido for vazio, termina a transmissão
+            if not data:
+                break
             
             # Escreve os dados no arquivo
             f.write(data)
